82c0096432ace90f297cfe96e36744555a7b1da1
Ryan Mulligan <hexagoncircle@gmail.com>
2021-11-06 10:05:32 -0700

    Updated theme script

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,21 +1,6 @@
 const images = document.querySelectorAll("img");
 const slider = document.querySelector(".theme-slider");
 
-const setThemeValue = (value) => {
-  document.documentElement.dataset.theme = value;
-  slider.value = value;
-};
-
-const getThemeValue = () => {
-  if (localStorage.theme) {
-    setThemeValue(localStorage.theme);
-    return;
-  }
-  if (window.matchMedia("(prefers-color-scheme: light)").matches) {
-    setThemeValue(5);
-  }
-};
-
 images.forEach((img) => {
   img.addEventListener("load", (event) => {
     event.target.removeAttribute("data-is-loading");
@@ -23,10 +8,9 @@ images.forEach((img) => {
 });
 
 document.documentElement.removeAttribute("data-no-js");
-
-getThemeValue();
+slider.value = document.documentElement.dataset.theme;
 
 slider.addEventListener("input", () => {
-  setThemeValue(slider.value);
+  document.documentElement.dataset.theme = slider.value;
   localStorage.setItem("theme", slider.value);
 });
