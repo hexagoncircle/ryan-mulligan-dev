dc00844dc41078963a8a5b69e4dca49cc5207d2e
Ryan Mulligan <hexagoncircle@gmail.com>
2022-07-06 18:37:41 -0700

    Update codepen images

--- .eleventy.js
+++ .eleventy.js
@@ -32,7 +32,7 @@ module.exports = function (eleventyConfig) {
         filenameFormat: function (id, src, width, format, options) {
           const extension = path.extname(src);
           const name = path.basename(src, extension).split("-")[0];
-          return `${name}-${width}w.${format}`;
+          return `codepen-${name}.${format}`;
         },
       });
     })();
