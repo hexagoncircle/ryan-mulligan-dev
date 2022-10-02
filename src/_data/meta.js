9f946920c123d930f7022d8fde8184619e1cde08
Ryan Mulligan <hexagoncircle@gmail.com>
2022-10-02 10:30:11 -0700

    Fix rss meta information

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -1,7 +1,7 @@
 module.exports = {
   lang: "en",
   url: process.env.URL || "http://localhost:8080",
-  siteName: "Ryan Mulligan",
+  title: "Ryan Mulligan",
   description:
     "Passenger through space and time, front-end web builder & bittersweet songs enthusiast",
   author: "Ryan Mulligan",
