24d67e44c4df09f62b6216ed314f5fe7e98d5823
Ryan Mulligan <hexagoncircle@gmail.com>
2023-11-23 06:31:43 -0800

    Refactor webmentions fetch and filters

--- 11ty/filters/webmentions.js
+++ 11ty/filters/webmentions.js
@@ -1,15 +1,50 @@
-const { domain } = require("../../src/_data/meta.js");
+const sanitizeHTML = require("sanitize-html");
 
 const getWebmentionsByType = (mentions, mentionType) => {
-  return mentions.filter((entry) =>
-    mentionType.split(", ").includes(entry["wm-property"])
-  );
+  return mentions.filter((entry) => mentionType.split(", ").includes(entry["wm-property"]));
 };
 
-const getWebmentionsForUrl = (webmentions, pathname) => {
-  const url = `https://${domain}${pathname}`;
+// const getWebmentionsForUrl = (webmentions, url) => {
+//   const commentTypes = ["in-reply-to", "mention-of", "like-of", "repose-of"];
+//   const allowedHTML = {
+//     allowedTags: ["b", "i", "em", "strong"],
+//   };
 
+//   const orderByDate = (a, b) => new Date(a.published) - new Date(b.published);
+
+//   const checkRequiredFields = (entry) => {
+//     const { author, published, content } = entry;
+//     return author.name && published && content;
+//   };
+
+//   const sanitizeContent = (entry) => {
+//     const { html, text } = entry.content;
+
+//     if (html) {
+//       entry.content.value =
+//         html.length > 2000 ? `mentioned in entry["wm-source"]` : sanitizeHTML(html, allowedHTML);
+//     } else {
+//       entry.content.value = sanitizeHTML(text, allowedHTML);
+//     }
+
+//     return entry;
+//   };
+
+//   return webmentions.children
+//     .filter((entry) => entry["wm-target"] === url)
+//     .filter((entry) => commentTypes.includes(entry["wm-property"]))
+//     .filter(checkRequiredFields)
+//     .sort(orderByDate)
+//     .map(sanitizeContent);
+// };
+
+const getWebmentionsForUrl = (webmentions, url) => {
   const commentTypes = ["in-reply-to", "mention-of"];
+  const allowedHTML = {
+    allowedTags: ["b", "i", "em", "strong"],
+  };
+
+  const orderByDate = (a, b) => new Date(a.published) - new Date(b.published);
 
   const hasRequiredFields = (entry) => {
     if (commentTypes.includes(entry["wm-property"])) {
@@ -19,9 +54,26 @@ const getWebmentionsForUrl = (webmentions, pathname) => {
     return entry;
   };
 
+  const sanitizeContent = (entry) => {
+    if (!entry.content) return entry;
+
+    const { html, text } = entry.content;
+
+    if (html) {
+      entry.content.output =
+        html.length > 2000 ? `mentioned in ${entry["wm-source"]}` : sanitizeHTML(html, allowedHTML);
+    } else {
+      entry.content.output = sanitizeHTML(text, allowedHTML);
+    }
+
+    return entry;
+  };
+
   const mentions = webmentions.children
     .filter((entry) => entry["wm-target"] === url)
-    .filter(hasRequiredFields);
+    .filter(hasRequiredFields)
+    .sort(orderByDate)
+    .map(sanitizeContent);
 
   return mentions;
 };
--- src/_data/webmentions.js
+++ src/_data/webmentions.js
@@ -1,98 +1,13 @@
-const unionBy = require("lodash/unionBy");
-const fs = require("fs");
-const { domain } = require("./meta.js");
-
-require("dotenv").config();
-
-const CACHE_DIR = ".cache";
-const API_ORIGIN = "https://webmention.io/api/mentions.jf2";
-const TOKEN = process.env.WEBMENTION_IO_TOKEN;
-
-async function fetchWebmentions(since, perPage = 10000) {
-  if (!domain) {
-    console.warn("> unable to fetch webmentions: no domain name specified");
-    return false;
-  }
-
-  if (!TOKEN) {
-    console.warn("> unable to fetch webmentions: no access token specified in environment.");
-    return false;
-  }
-
-  let url = `${API_ORIGIN}?domain=${domain}&token=${TOKEN}&sort-dir=up&per-page=${perPage}`;
-
-  if (since) url += `&since=${since}`;
-
-  const response = await fetch(url);
-
-  if (response.ok) {
-    const feed = await response.json();
-    console.log(`> ${feed.children.length} new webmentions fetched from ${API_ORIGIN}`);
-    return feed;
-  }
-
-  return null;
-}
-
-// Merge fresh webmentions with cached entries, unique per id
-function mergeWebmentions(a, b) {
-  return unionBy(a.children, b.children, "wm-id");
-}
-
-// save combined webmentions in cache file
-function writeToCache(data) {
-  const filePath = `${CACHE_DIR}/webmentions.json`;
-  const fileContent = JSON.stringify(data, null, 2);
-
-  // create cache folder if it doesnt exist already
-  if (!fs.existsSync(CACHE_DIR)) {
-    fs.mkdirSync(CACHE_DIR);
-  }
-
-  // write data to cache json file
-  fs.writeFile(filePath, fileContent, (err) => {
-    if (err) throw err;
-    console.log(`> webmentions saved to ${filePath}`);
-  });
-}
-
-// get cache contents from json file
-function readFromCache() {
-  const filePath = `${CACHE_DIR}/webmentions.json`;
-
-  if (fs.existsSync(filePath)) {
-    console.log("Reading webmentions from cache dir");
-    const cacheFile = fs.readFileSync(filePath);
-    return JSON.parse(cacheFile);
-  }
-
-  // no cache found.
-  console.log("No webmentions cache found");
-
-  return {
-    lastFetched: null,
-    children: [],
-  };
-}
+const EleventyFetch = require("@11ty/eleventy-fetch");
 
 module.exports = async function () {
-  const cache = readFromCache();
-  if (cache.children.length) {
-    console.log(`> ${cache.children.length} webmentions loaded from cache`);
-  }
+  const url = `https://webmention.io/api/mentions.jf2?token=${process.env.WEBMENTION_IO_TOKEN}&per-page=1000`;
+  const res = EleventyFetch(url, {
+    duration: "1h",
+    type: "json",
+  });
 
-  // Only fetch new mentions in production
-  const feed = await fetchWebmentions(cache.lastFetched);
+  const webmentions = await res;
 
-  if (feed) {
-    const webmentions = {
-      lastFetched: new Date().toISOString(),
-      children: mergeWebmentions(cache, feed),
-    };
-
-    writeToCache(webmentions);
-    return webmentions;
-  }
-
-  return cache;
+  return webmentions;
 };
